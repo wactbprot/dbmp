b9d738da0dadeac5c7167a7aad740b249113e795
wactbprot <thsteinbock@web.de>
2014-04-25 20:00:25 +0200

    + testdoc

---
+++ docs/mpdef/Mp.Obj/Container.arr.js
@@ -0,0 +1,60 @@
+[
+    {
+        "Ctrl": "unformed",
+        "Elements": [
+            {
+                "type": "div",
+                "html": [
+                    {
+                        "type": "h3",
+                        "html": "Testify"
+                    }
+                ]
+            }
+        ],
+        "NoOfRepeats": 1,
+        "Recipe": [
+            [
+                "Mp_wait_long",
+                "Mp_wait_short",
+                "Mp_wait_long"
+            ],
+            [
+                "Mp_wait_long"
+            ],
+            [
+                "Mp_wait_short"
+            ]
+        ],
+        "Title": "Container 1"
+    },
+    {
+        "Ctrl": "unformed",
+        "Elements": [
+            {
+                "type": "div",
+                "html": [
+                    {
+                        "type": "h3",
+                        "html": "Testify"
+                    }
+                ]
+            }
+        ],
+        "NoOfRepeats": 1,
+        "Recipe": [
+            [
+                "Mp_wait_long",
+                "Mp_wait_short",
+                "Mp_wait_long"
+            ],
+            [
+                "Mp_wait_long"
+            ],
+            [
+                "Mp_wait_short"
+            ]
+        ],
+        "Title": "Container 2"
+    }
+]
---
+++ docs/mpdef/Mp.Obj/Tasks.arr.js
@@ -0,0 +1,18 @@
+[
+    {
+        "Action": "yamp_worker_wait",
+        "Comment": "Verzögert den Programmablauf um _longwait ms",
+        "TaskName": "wait_long",
+        "Value": {
+            "WaitTime": "_longwait"
+        }
+    },
+    {
+        "Action": "yamp_worker_wait",
+        "Comment": "Verzögert den Programmablauf um _shortwait ms",
+        "TaskName": "wait_short",
+        "Value": {
+            "WaitTime": "_shortwait"
+        }
+    }
+]
