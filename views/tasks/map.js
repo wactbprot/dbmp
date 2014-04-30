94019b1087bcced6825cf906369ce7f1154a11ef
wactbprot <thsteinbock@web.de>
2014-04-30 12:27:00 +0200

    replace the full taskname before return 	modified:   docs/mpdef/_rev.str.txt 	modified:   views/tasks/map.js

--- views/tasks/map.js
+++ views/tasks/map.js
@@ -9,26 +9,28 @@ function(doc) {
      doc.Mp.Tasks){
     var dMT  = doc.Mp.Tasks,
         dMD  = doc.Mp.Defaults,
-        name = "Mp";
+        name = "Mp",
+        intask,
+        strtask,
+        outtask,
+        key;
 
     for(var i = 0; i < dMT.length; i++){
+      intask  = dMT[i];
+      key     = name + "_" + intask.TaskName.replace(/\s/g, "_");
 
-      var task = dMT[i],
-          key  = name+"_"+task.TaskName.replace(/\s/g, "_");
-
-      task.TaskName = key;
-
-      var strtask =JSON.stringify(task)
-
+      strtask = JSON.stringify(intask)
       if(typeof dMD === "object"){
         for(var d in dMD){
-          var patt=new RegExp( d ,"g");
-
-          strtask = strtask.replace(patt, dMD[d]);
+          var patt = new RegExp( d ,"g");
+          strtask  = strtask.replace(patt, dMD[d]);
         }
       }
 
-      emit(key, JSON.parse(strtask))
+      outtask = JSON.parse(strtask)
+      outtask.TaskName = key
+
+      emit(key, outtask)
     }
   }
 }
