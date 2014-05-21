b9d3f676937ca4359a24a4d3d9beae7705d015e8
wactbprot <thsteinbock@web.de>
2014-05-21 10:26:35 +0200

    more complex default repacement

--- views/tasks/map.js
+++ views/tasks/map.js
@@ -5,28 +5,41 @@ function(doc) {
  * emit them
  *
  */
-  if(doc.Mp &&
-     doc.Mp.Tasks){
-    var dMT  = doc.Mp.Tasks,
-        dMD  = doc.Mp.Defaults,
-        name = "Mp",
-        intask,
-        strtask,
-        outtask,
-        key;
+var tob,
+    i,
+    j,
+    intask,
+    strtask,
+    outtask,
+    key;
 
-    for(var i = 0; i < dMT.length; i++){
-      intask  = dMT[i];
-      key     = name + "_" + intask.TaskName.replace(/\s/g, "_");
+
+  if(doc.Mp) tob = doc.Mp;
+  if(doc.CalibrationObject) tob = doc.CalibrationObject;
+
+  if(tob &&
+     tob.Tasks){
+
+    var t    = tob.Tasks,
+        d    = tob.Defaults,
+        name = tob.Name;
+
+    for(i = 0; i < t.length; i++){
+      intask  = t[i];
+
+      key     = name + "-" + intask.TaskName.replace(/\s/g, "_");
 
       strtask = JSON.stringify(intask)
-      if(typeof dMD === "object"){
-        for(var d in dMD){
-          var patt = new RegExp( d ,"g");
-          strtask  = strtask.replace(patt, dMD[d]);
+
+      if(typeof d === "object"){
+        for(j in d){
+
+          var patt = new RegExp( j ,"g");
+          strtask  = strtask.replace(patt, d[j]);
+          strtask  = strtask.replace(/\n/g, "\\n");
+          strtask  = strtask.replace(/\r/g, "\\r");
         }
       }
-
       outtask = JSON.parse(strtask)
       outtask.TaskName = key
 
