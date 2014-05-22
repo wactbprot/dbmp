64b86d13299b7928027173a0427defbb7434e360
wactbprot <thsteinbock@web.de>
2014-05-22 09:04:19 +0200

    cdg test tasks

--- docs/mpdef/Mp.obj.js
+++ docs/mpdef/Mp.obj.js
@@ -1,168 +1,196 @@
 {
-  "Name": "Mp",
-  "Title": "ssmp",
-  "Container": [
-    {
-      "Ctrl": "unformed",
-      "Element": [
+    "Name": "Mp",
+    "Title": "ssmp",
+    "Container": [
         {
-          "type": "div",
-          "html": [
-            {
-              "type": "h3",
-              "html": "Testify"
-            }
-          ]
-        }
-      ],
-      "NoOfRepeats": 1,
-      "Recipe": [
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 1000,
-             "_waitfor" : "Waiting for Godot "}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 3000}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 500}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 550}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 605}
-          }
-        ]
-      ],
-      "Title": "Container 1"
-    },
-    {
-      "Ctrl": "unformed",
-      "Element": [
-        {
-          "type": "div",
-          "html": [
-            {
-              "type": "h3",
-              "html": "Testify"
-            }
-          ]
-        }
-      ],
-      "NoOfRepeats": 1,
-      "Recipe": [
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 3300}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 3400}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 500}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 400}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 800}
-          }
-        ]
-      ],
-      "Title": "Container 2"
-    }
-  ],
-  "Date": [
-    {
-      "Type": "created",
-      "Value": "2014-04-04"
-    }
-  ],
-  "Defaults": {
-    "_waittime": 1000,
-    "_waitfor" : "Ready in",
-    "_waitunit" : "ms"
-  },
-  "Recipes": [
-    {
-      "Conditions": [
-        {
-          "ReadFrom": "token_1",
-          "Methode": "LT",
-          "Params": [
-            "value_1",
-            0.0001
-          ]
+            "Ctrl": "unformed",
+            "Element": [
+                {
+                    "type": "div",
+                    "html": [
+                        {
+                            "type": "h3",
+                            "html": "Testify"
+                        }
+                    ]
+                }
+            ],
+            "NoOfRepeats": 1,
+            "Recipe": [
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 1000,
+                            "_waitfor": "Waiting for Godot "
+                        }
+                    },
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 3000
+                        }
+                    },
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 500
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 550
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 605
+                        }
+                    }
+                ]
+            ],
+            "Title": "Container 1"
         },
         {
-          "ReadFrom": "token_1",
-          "Methode": "GT",
-          "Params": [
-            "value_1",
-            1e-11
-          ]
+            "Ctrl": "unformed",
+            "Element": [
+                {
+                    "type": "div",
+                    "html": [
+                        {
+                            "type": "h3",
+                            "html": "Testify"
+                        }
+                    ]
+                }
+            ],
+            "NoOfRepeats": 1,
+            "Recipe": [
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 3300
+                        }
+                    },
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 3400
+                        }
+                    },
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 500
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 400
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 800
+                        }
+                    }
+                ]
+            ],
+            "Title": "Container 2"
         }
-      ],
-      "Recipe": [
-  [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 1000}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 3000}
-          },
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 500}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 1000}
-          }
-        ],
-        [
-          {"TaskName":"Mp-wait",
-           "Replace":{
-             "_waittime": 1000}
-          }
-        ]
-      ],
-      "RecipeClass": "choose",
-      "ShortDescr": "Warten\n"
-    }
-  ],
-  "Tasks": [
-    {
-      "Action": "wait",
-      "Comment": "_waitfor  _waittime ms",
-      "TaskName": "wait",
-      "Value": {
-        "WaitTime": "_waittime"
-      }
-    }
-  ]
+    ],
+    "Date": [
+        {
+            "Type": "created",
+            "Value": "2014-04-04"
+        }
+    ],
+    "Defaults": {
+        "_waittime": 1000,
+        "_waitfor": "Ready in",
+        "_waitunit": "ms"
+    },
+    "Recipes": [
+        {
+            "Conditions": [
+                {
+                    "ReadFrom": "token_1",
+                    "Methode": "LT",
+                    "Params": [
+                        "value_1",
+                        0.0001
+                    ]
+                },
+                {
+                    "ReadFrom": "token_1",
+                    "Methode": "GT",
+                    "Params": [
+                        "value_1",
+                        1e-11
+                    ]
+                }
+            ],
+            "Recipe": [
+                [
+                    {
+                        "TaskName": "FM3_1T-device_init"
+                    },
+                    {
+                        "TaskName": "FM3_1T-range_init",
+                        "Replace": {
+                            "_range": "X1"
+                        }
+                    },
+                    {
+                        "TaskName": "FM3_1T-slope_exec",
+                        "Replace": {
+                            "_prefix": "drift",
+                            "_docpath": "Calibration.Measurement.Values.Drift"
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 1000
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "Mp-wait",
+                        "Replace": {
+                            "_waittime": 1000
+                        }
+                    }
+                ]
+            ],
+            "RecipeClass": "choose",
+            "ShortDescr": "Warten\n"
+        }
+    ],
+    "Tasks": [
+        {
+            "Action": "wait",
+            "Comment": "_waitfor  _waittime ms",
+            "TaskName": "wait",
+            "Value": {
+                "WaitTime": "_waittime"
+            }
+        }
+    ]
 }
