91c664d105c5bdc9bc154ee8af8c2699bfc86803
wactbprot <thsteinbock@web.de>
2014-05-28 11:09:20 +0200

    bu task mp task ok task 	modified:   lists/get.js

--- lists/get.js
+++ lists/get.js
@@ -1,10 +1,49 @@
 function(head, req) {
-  var task, row, j,
-      tr = (req.body === "undefined") ? false: true,
-      b  = tr ? JSON.parse(req.body) :{};
-  log(tr)
+  var oktask,
+      butask,
+      mptask,
+      row,
+      j,
+      task,
+      tr      = (req.body === "undefined") ? false: true,
+      b       = tr ? JSON.parse(req.body) :{},
+      repl    = b.Replace,
+      isCuCo  = b.CuCo,
+      tn      = b.TaskName,
+      dn      = b.DeviceName,
+      mn      = b.MpName,
+      mptn    = "",
+      butn    = "";
+
+  if(dn) butn = tn.replace(dn,"CUCO")
+  if(dn && mn) mptn = tn.replace(dn, mn)
+
   while(row = getRow()) {
-    task = row.value;
+
+    if(row.value.TaskName === tn) {
+      oktask = row.value;
+    }
+    if(isCuCo &&
+       row.value.TaskName === mptn){
+      mptask = row.value;
+    }
+    if(isCuCo &&
+       row.value.TaskName === butn){
+      butask = row.value;
+    }
+  }
+
+  if(!oktask){
+    if(!mptask){
+      if(!butask){
+      }else{
+        task = butask;
+      }
+    }else{
+      task = mptask;
+    }
+  }else{
+    task = oktask;
   }
 
   if(task &&
@@ -22,8 +61,8 @@ function(head, req) {
         for(j in d){
           var patt = new RegExp( j ,"g");
 
-          if(tr && b[j]){
-            strtask  = strtask.replace(patt, b[j]);
+          if(tr && repl && repl[j]){
+            strtask  = strtask.replace(patt, repl[j]);
             d[j]     = b[j];
           }else{
             strtask  = strtask.replace(patt, d[j]);
