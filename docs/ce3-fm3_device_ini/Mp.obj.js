d864c1cf93f2f4ace042cae63af9115b94afd207
wactbprot <thsteinbock@web.de>
2014-08-14 17:35:53 +0200

    docs/ce3-fm3_device_ini/

---
+++ docs/ce3-fm3_device_ini/Mp.obj.js
@@ -0,0 +1,44 @@
+{
+    "Container": [
+        {
+            "Element": [
+                "Documents"
+            ],
+            "Description": "testdescr container 0",
+            "Ctrl": "load;run",
+            "NoOfRepeats": 1,
+            "Definition": [
+                [
+                    {
+                        "TaskName": "FM3_1T-device_ini",
+                        "Replace": {
+                            "_exchpath": "FM3_1T_ini_ok.Value.value"
+                        }
+                    }
+                ],
+                [
+                    {
+                        "TaskName": "FM3_1T-set_range",
+                        "Replace": {
+                            "_range": "X1",
+                            "_exchpath": "FM3_1T_range_ok.Value.value"
+                        }
+                    }
+                ]
+            ],
+            "Title": "Initialisieren aller Geräte"
+        }
+    ],
+    "Exchange": {
+        "FM3_1T_ini_ok": {
+            "Value": {
+                "value": false
+            }
+        },
+        "FM3_1T_range_ok": {
+            "Value": {
+                "value": false
+            }
+        }
+    }
+}
