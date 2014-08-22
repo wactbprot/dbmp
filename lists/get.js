d5cc9f07bb73244562128922ac525039c0dbfa8e
wactbprot <thsteinbock@web.de>
2014-08-22 11:39:18 +0200

    Ersetzung auch wenn im Quelldokument der task kein Defaults definiert ist 	modified:   lists/get.js

--- lists/get.js
+++ lists/get.js
@@ -1,5 +1,5 @@
 function(head, req) {
-  var oktask, mptask, row, i, j, task, mptn, butn,
+  var oktask, mptask, row, i, j, task, mptn, butn, def,
       tr      = (req.body === "undefined") ? false : true,// try replace
       b       = tr ? JSON.parse(req.body) : {}, // body
       tn      = b.TaskName, //
@@ -11,7 +11,7 @@ function(head, req) {
       d       = new Date();
 
   tn = req.query.taskname ? JSON.parse(req.query.taskname) : tn;
-  if(dn && mn) mptn = tn.replace(dn, mn)
+  if(dn && mn) mptn = tn.replace(dn, mn);
 
   while(row = getRow()) {
     if(row.value.TaskName === tn) {
@@ -46,48 +46,45 @@ function(head, req) {
     }
 
     // Defaults Ersetzungen
-    if(task.Defaults && typeof task.Defaults === "object"){
-
-      var def = JSON.parse(JSON.stringify(task.Defaults)); // internal defaults
+    if(task.Defaults &&
+       typeof task.Defaults === "object"){
+      def = JSON.parse(JSON.stringify(task.Defaults)); // internal defaults
       delete task.Defaults
-
-      // Defaults um Replaces erweitern
-      // bzw. ersetzen
-      if(repl && typeof repl === "object"){
-        for(i in repl){
-          def[i] = repl[i];
-        }
-      }
-      // _mpname
-      // &
-      // _devicename
-      // steht _standardmäßig
-      // zur Verfügung
-      def["@mpname"]     = mn;
-      def["@devicename"] = dn;
-      def["@year"]       = d.getFullYear();
-
-      var strtask = JSON.stringify(task);
-      for(j in def){
-
-        var patt = new RegExp( j ,"g");
-
-        if(Object.prototype.toString.call( def[j] ) === '[object Array]'){
-          strtask = strtask.replace(patt, JSON.stringify(def[j]))
-                    .replace(/\"\[/g, "\[")
-                    .replace(/\]\"/g, "\]");
-        }else{
-          strtask  = strtask.replace(patt, def[j]);
-        }
-        strtask  = strtask.replace(/\n/g, "\\n");
-        strtask  = strtask.replace(/\r/g, "\\r");
-
-
-      }
-
-      task          = JSON.parse(strtask);
-      task.Defaults = def;
+    }else{
+      def = {};
     }
+    // @mpname, @devicename, @year
+    // stehen in den task-Definitionen
+    // immer zur Verfügung
+    def["@mpname"]     = mn;
+    def["@devicename"] = dn;
+    def["@year"]       = d.getFullYear();
+
+
+    // Defaults um Replaces erweitern
+    // bzw. ersetzen
+    if(repl && typeof repl === "object"){
+      for(i in repl){
+        def[i] = repl[i];
+      }
+    }
+
+    var strtask = JSON.stringify(task);
+    for(j in def){
+      var patt = new RegExp( j ,"g");
+      if(Object.prototype.toString.call( def[j] ) === '[object Array]'){
+        strtask = strtask.replace(patt, JSON.stringify(def[j]))
+                  .replace(/\"\[/g, "\[")
+                  .replace(/\]\"/g, "\]");
+      }else{
+        strtask  = strtask.replace(patt, def[j]);
+      }
+      strtask  = strtask.replace(/\n/g, "\\n");
+      strtask  = strtask.replace(/\r/g, "\\r");
+    }
+
+    task          = JSON.parse(strtask);
+    task.Defaults = def;
 
     if(id && typeof id === "object"){
       task.Id     = id;
