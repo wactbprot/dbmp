05183b27f62cd1b72e8258e17e77338094e213d8
wactbprot <thsteinbock@web.de>
2014-08-28 11:35:24 +0200

    mps with val

--- views/mps/map.js
+++ views/mps/map.js
@@ -1,7 +1,10 @@
 function(doc) {
   if(doc.Mp){
     var dm  = doc.Mp,
-        key = dm.Standard || "";
-    emit(key,null)
+        key = dm.Standard || "",
+        val = {"Name":        dm.Name        || "__name__",
+               "Describtion": dm.Description || "__describtion__",
+               "Standard":    dm.Standard    || "__standard__"}
+    emit(key, val)
   }
 }
