f33ea1d42df08d549be80c66cae3febcc2183139
wactbprot <thsteinbock@web.de>
2015-05-21 15:05:37 +0200

    select cd to new struct

--- views/calibrations/map.js
+++ views/calibrations/map.js
@@ -8,18 +8,18 @@ function(doc) {
   if(doc && doc.Calibration){
     var dc      = doc.Calibration
       , dcm     = dc.Measurement
-      , dcp     = dc.Presettings
-      , dcpt    = dcp.ToDo     ||{}
-      , dcpc    = dcp.Customer ||{}
-      , dccuco  = dc.CalibrationObject[0] ||{}
-      , dcsm    = dc.Standard ? dc.Standard.Name : "_X_";
+      , dct     = dc.ToDo     || {}
+      , dcc     = dc.Customer || {}
+      , dcsm    = dct.Standard
+      , dccuco  = dc.CalibrationObject[0] || {}
+
 
     emit(dcsm + "-" + dc.Year
         , { Id       : doc._id
-          , Sign     : dc.Sign       || "__sign__"
-          , Year     : dc.Year       || "__year__"
-          , Customer : dcpc.Name     || "__customer_name__"
+          , Sign     : dc.Sign      || "__sign__"
+          , Year     : dc.Year      || "__year__"
+          , Customer : dcc.Name     || "__customer_name__"
           , Device   : dccuco.Name  || "__device_name__"
-          , ToDoType : dcpt.Type     || "__todo_type__" });
+          , ToDoType : dct.Type     || "__todo_type__" });
   }
 }
