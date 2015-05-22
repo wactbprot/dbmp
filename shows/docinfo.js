f27036a490d1a8ed9ebcc93ea03d31f2803952a0
wactbprot <thsteinbock@web.de>
2015-05-22 15:48:32 +0200

    merge marks

--- shows/docinfo.js
+++ shows/docinfo.js
@@ -2,49 +2,36 @@ function (doc, req) {
   var res = {DocInfo:true};
 
   if(doc && doc.Calibration){
-<<<<<<< HEAD
-    var dc      = doc.Calibration,
-        dcm     = dc.Measurement,
-        dcp     = dc.Presettings,
-        dcpt    = dcp.ToDo,
-        dcpc    = dcp.Customer,
-        dccuco  = dc.CalibrationObject[0];
 
-    if(dcpt && dcpc && dccuco){
-      res.id         = doc._id;
-      res.Sign       = dc.Sign;
-      res.Year       = dc.Year;
-      res.Customer   = dcpc.Name;
-      res.Device     = dccuco.Name;
-      res.ToDoType   = dcpt.Type
-=======
     var dc   = doc.Calibration
-      , dcp  = dc.Presettings
+      , dct  = dc.ToDo
+      , dcc  = dc.Customer
       , dcco = dc.CalibrationObject
 
     res.id    = doc._id;
     res.Sign  = dc.Sign;
     res.Year  = dc.Year;
-
-    if(dcp){
-      var dcpt     = dcp.ToDo || {}
-        , dcpc     = dcp.Customer || {};
-
-      res.Customer = dcpc.Name  || "~";
-      res.ToDoType = dcpt.Type  || "~"
+    
+    if(dct){
+      res.ToDoType = dct.Type  || "~"
     }else{
-      res.warn  = "ToDo or Customer missing";
+      res.warn  = "missing ToDo";
+    }
+
+    if(dcc){
+      res.Customer = dcc.Name  || "~";
+    }else{
+      res.warn  = "missing Customer";
     }
 
     if(dcco && dcco[0]){
-      res.Device = dccu[0].Name|| "~";
->>>>>>> ea57d9441fdbdcebff4670a605a30ea38bf93b34
+      res.Device = dcco[0].Name|| "~";
     }else{
       res.warn  = "CalibrationObject missing ";
     }
+
   }else{
     res.error  = "not a Calibration";
   }
-
   return toJSON(res);
 };
