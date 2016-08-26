0b28b6c56f0f8e8d08a5340e310d4bbbc45f471c
wactbprot <thsteinbock@web.de>
2016-08-26 15:32:21 +0200

    plot page

--- shows/data.js
+++ shows/data.js
@@ -1,8 +1,9 @@
 // http://localhost:5984/vl_db/_design/dbmp/_show/plotdata/cal-2016-ce3-kk-75013_0001?x=Analysis:Pressure:ind&y=Analysis:Error:relative
 function (doc, req) {
-  var x, y, ret;
+  var x, y, RX, RY, ret;
   if(doc && doc.Calibration){
-    var dc = doc.Calibration;
+    var dc = doc.Calibration
+      , id = doc._id;
     if(req.query.x){
       x = req.query.x;
     }else{
@@ -14,22 +15,89 @@ function (doc, req) {
     }else{
       y = "Analysis:Error:relative"
     }
+
     x = x.split(":")
     y = y.split(":")
-    if(x.length == 3 && y.length == 3){
-      if(dc[x[0]] &&
-         dc[x[0]]["Values"][x[1]] &&
-         dc[x[0]]["Values"][x[1]]&&
-         dc[y[0]] &&
-         dc[y[0]]["Values"][y[1]] &&
-         dc[y[0]]["Values"][y[1]]){
-        ret = dc[y[0]]["Values"][y[1]]
-      }
-        }else{
-      ret = { "error": "wrong request params"
-            , "reason":"too short"}
-    }
-    return toJSON(ret);
 
+    var xsource = x[0]
+      , ysource = y[0]
+      , xquant  = x[1]
+      , yquant  = y[1]
+      , xtype   = x[2]
+      , ytype   = y[2]
+
+
+    if(x.length == 3 && y.length == 3){
+      if(dc[xsource] &&
+         dc[xsource]["Values"][xquant] &&
+         dc[xsource]["Values"][xquant]&&
+         dc[ysource] &&
+         dc[ysource]["Values"][yquant] &&
+         dc[ysource]["Values"][yquant]){
+        var X = dc[xsource]["Values"][xquant]
+          , Y = dc[ysource]["Values"][yquant]
+
+        // X
+        if( Object.prototype.toString.call( X ) === '[object Array]' ) {
+          var NX = X.length;
+          for(var i = 0; i < NX; i++){
+            if(X[i].Type == xtype){
+              RX = X[i];
+              break;
+            }
+          }
+        }else{
+          if(X.Type == xtype){
+            RX = X;
+          }
+        }
+        // Y
+        if( Object.prototype.toString.call( Y ) === '[object Array]' ) {
+          var NY = Y.length;
+          for(var i = 0; i < NY; i++){
+            if(Y[i].Type == ytype){
+              RY = Y[i];
+              break;
+            }
+          }
+        }else{
+          if(Y.Type == ytype){
+            RY = Y;
+          }
+        }
+        if(typeof RX == 'object' &&  typeof RY == 'object'){
+          var N = RY.Value.length
+          if(RY.Value.length == N){
+            var axis = {x:{label: "(" + xsource +")" + xquant + "[" + xtype +"]" },
+                        y:{label: "(" + ysource +")" + yquant + "[" + ytype +"]"}}
+              , d = []
+            for(var j = 0; j < N; j++){
+              d.push([RX.Value[j], RY.Value[j]]);
+            }
+            ret =  {data:[ {data : d, label : id, points : { show : true }}],
+                   options:{legend : { position : 'se', backgroundColor : '#D2E8FF' },
+                            title : ysource + "/" + yquant + " vs. "  + xsource + "/" + xquant,
+                            xaxis:{scaling: 'logarithmic'} // => Scaling, can be 'linear' or 'logarithmic'
+                           }
+                  }
+          }else{
+            ret = { "error": "data error"
+                  , "reason":"x an y values do not have the same length"}
+          }
+
+        }else{
+          ret = { "error": "wrong request params"
+                , "reason":"nothing in the path"}
+        }
+      }
+    }else{
+      ret = { "error": "wrong request params"
+            , "reason":"wrong path"}
+    }
+  }else{
+    ret = { "error": "wrong request params"
+          , "reason":"too short"}
   }
+
+  return toJSON(ret);
 };
