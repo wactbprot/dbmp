b7f7b41e75310e1941daf2bdf7666a794f3ff927
wactbprot <thsteinbock@web.de>
2017-07-13 11:04:09 +0200

    better/cleaner docinfo

--- shows/docinfo.js
+++ shows/docinfo.js
@@ -1,54 +1,35 @@
-
 function (doc, req) {
-  var res = {DocInfo:true}
-  var valid = false;
+
+  var res = {DocInfo:false}
 
   if(doc && doc.Calibration){
-    var  dc   = doc.Calibration
+    var dc   = doc.Calibration
       , dct   = dc.ToDo
       , dcc   = dc.Customer
-      , dccu  = dc.CustomerObject
+      , dccu  = dc.CustomerObject;
 
-    valid = true;
-
+    res.DocInfo      = true
     res.Id           = doc._id;
     res.Certificate  = dc.Certificate;
     res.Issue        = dc.Issue;
     res.Year         = dc.Year;
 
-    if(dct && dct.Type){
-      res.ToDoType = dct.Type;
-    }else{
-      res.warn  = "missing ToDo";
-    }
-
-    if(dcc && dcc.Name){
-      res.Customer = dcc.Name;
-    }else{
-      res.warn  = "missing Customer";
-    }
-
-    if(dccu && dccu.Name){
-      res.Device = dccu.Name;
-    }else{
-      res.warn  = "CalibrationObject missing ";
-    }
+    res.ToDoType = dct && dct.Type ? dct.Type : "no type";
+    res.Customer = dcc && dcc.Name ? dcc.Name : "no customer" ;
+    res.Device   = dccu && dccu.Name ? dccu.Name : "no device name" ;
   }
 
   if(doc && doc.Measurement){
+    res.DocInfo     = true;
     res.Id          = doc._id;
     res.Measurement = true;
-    valid           = true;
   }
 
   if(doc && doc.Commissioning){
     res.Id            = doc._id;
     res.Commissioning = true;
-    valid             = true;
+    res.DocInfo       = true;
   }
 
-  if(!valid){
-    res.error  = "not a calibration-, measurement or commissioning document";
-  }
   return toJSON(res);
 };
